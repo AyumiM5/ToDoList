02c2671dd4d1dbfe98c537dec513d4c79381b5e2
AyumiM5 <mmmiray.555@gmail.com>
2021-12-22 08:45:35 +0000

    [Add]ListNew

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ApplicationRecord
   belongs_to :user
+  validates :title, length: {minimun: 2, maximum: 250}
 end
--- db/migrate/20211222060403_create_lists.rb
+++ db/migrate/20211222060403_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :title, null: false
-      t.references :user, foreign_key: true, null: failse
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
